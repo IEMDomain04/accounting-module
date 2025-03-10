5adc0eecc8231e52e1226a10d0726e6a67a226ee
IEMDomain04.dc <emmanman0011@gmail.com>
2025-03-10 20:04:30 +0800

    ChartOfAccounts Hardcoded - Reusable Components is on process

--- src/modules/Accounting/submodules/ChartOfAccounts.jsx
+++ src/modules/Accounting/submodules/ChartOfAccounts.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import "../styles/ChartOfAccounts.css";
+import SearchBar from "../../../shared/components/SearchBar";
 
 const BodyContent = () => {
     return (
-        <div className="genled">
+        <div className="chartAccounts">
             <div className="body-content-container">
-                <p>Hello General Ledger SubModule!</p>
-                <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.genled' at the start.</p>
+
+                {/* Input Fields, Buttons, and Sorting */}
+                <div className="component-container">
+                    <div className="select-account-dropdown">
+                        <select className="accounts-dropdown">
+                            <option value="cash-on-hand">Cash on Hand</option>
+                            <option value="account-receivables">Account Receivables</option>
+                            <option value="cash-in-bank">Cash in Bank</option>
+                        </select>
+                    </div>
+
+                    <div className="input-field-container">
+                        <SearchBar />
+                    </div>
+
+                    <div className="buttons-container">
+                        <button className="Button1"> Add account </button>
+                        <button className="Button2"> Submit </button>
+                        <button className="Button1"> Archive </button>
+                    </div>
+                </div>
+
+                {/* Table */}
+                <table className="accounts-table">
+                    <thead>
+                        <tr>
+                            <th>Account Type</th>
+                            <th>Account Name</th>
+                            <th>Date Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>test</td>
+                            <td>test</td>
+                            <td>test</td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
         </div>
 
