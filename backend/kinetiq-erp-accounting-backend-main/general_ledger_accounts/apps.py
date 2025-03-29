bdecaa8f9685b8d192f8185695241f0049d9f9d6
IEMDomain04 <emmanman0011@gmail.com>
2025-03-28 22:59:37 -0700

    Removed unecessary code in General Ledger

--- backend/kinetiq-erp-accounting-backend-main/general_ledger_accounts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeneralLedgerAccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'general_ledger_accounts'
--- src/modules/Accounting/submodules/GeneralLedgerAccounts.jsx
+++ src/modules/Accounting/submodules/GeneralLedgerAccounts.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Accounting-Global-Styling.css';
 import Forms from '../components/Forms';
 import Table from '../components/Table';
 
 const GeneralLedgerAccounts = () => {
   const columns = ["GL Account ID", "Account name", "Account code", "Account ID", "Status", "Created at.."];
+  const [data, setData] = useState([]);
 
-  const data = [
-    ["GL-101", "Petty Cash", "1010", "ACC-101", "Active", "2024-02-18"],
-    ["GL-102", "Bank Account", "1020", "ACC-102", "Active", "2024-03-01"],
-    ["GL-103", "Office Equipment", "1501", "ACC-103", "Inactive", "2023-12-30"],
-    ["GL-104", "Loan Payable", "2101", "ACC-104", "Active", "2024-01-22"],
-    ["GL-105", "Consulting Revenue", "4201", "ACC-105", "Active", "2023-11-10"]
-  ];
+  const fetchData = () => {
+    fetch('http://127.0.0.1:8000/api/general-ledger-accounts/').then(response => response.json())
+  }
 
   return (
     <div className="generalLedger">
